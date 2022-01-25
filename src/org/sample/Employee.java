b83707b70fd3cfde5bccf6eb3d162f06266c620d
Karthick9709 <karthickpalani9709@gmail.com>
2022-01-25 15:52:01 +0530

    vignesh done

--- src/org/sample/Employee.java
+++ src/org/sample/Employee.java
@@ -1,6 +1,9 @@
 package org.sample;
 
 public class Employee {
+	private void m2() {
+		System.out.println("m2 is created by vignesh");
+	}
 	public static void main(String[] args) {
 		int a=10;
 		System.out.println("a value:"+a);
