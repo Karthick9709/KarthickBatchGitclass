3e8db9437ec19ff02ae080ee48ae5dc224d14e26
Karthick9709 <karthickpalani9709@gmail.com>
2022-01-26 16:10:46 +0530

    mohan done his work

--- src/org/sample/Employee.java
+++ src/org/sample/Employee.java
@@ -1,6 +1,9 @@
 package org.sample;
 
 public class Employee {
+	private void m3() {
+		System.out.println("mohan done his work");
+	}
 	public static void main(String[] args) {
 		int a=10;
 		System.out.println("a value:"+a);
